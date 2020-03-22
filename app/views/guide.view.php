7f8d30882467311ee19b7c1318a7c96039da2c2c
Muhammad A. Mwinchande <ammwinchande@gmail.com>
2020-03-22 18:57:58 +0300

    completed index and guide pages

---
+++ app/views/guide.view.php
@@ -0,0 +1,21 @@
+<?php require 'partials/header.php'; ?>
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h2>System Guide</h2>
+			<p>
+				System Guide goes here!
+			</p>
+			<hr>
+		</div>
+	</div>
+	<div class="row mt-5">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h3>Application Links:</h3>
+			<ul>
+				<li><a href="/customer">Manage Customers</a></li>
+				<li><a href="/gender">Manage Genders</a></li>
+			</ul>
+		</div>
+	</div>
+
+<?php require 'partials/footer.php'; ?>
---
+++ app/views/index.view.php
@@ -0,0 +1,23 @@
+<?php require 'partials/header.php'; ?>
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h2>Intro:</h2>
+			<p>
+				Ongeza Customer Management System allows you to manage customers, and genders. Managing customers means ability to create, view, edit, and delete customers. Also, manage gender means ability to create, view, edit, delete genders that are to be used by the customers.
+
+				This is a part of Interview Task from Ongeza Tanzania Ltd.
+			</p>
+			<hr>
+		</div>
+	</div>
+	<div class="row mt-5">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h3>Application Links:</h3>
+			<ul>
+				<li><a href="/customer">Manage Customers</a></li>
+				<li><a href="/gender">Manage Genders</a></li>
+			</ul>
+		</div>
+	</div>
+
+<?php require 'partials/footer.php'; ?>
