9c8a029918b64aa9807132d308f00972e9cf70f1
Muhammad A. Mwinchande <ammwinchande@gmail.com>
2020-03-22 19:01:46 +0300

    added application bootstrapping for all batteries

---
+++ core/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Core\App;
+
+App::bind('config', require 'config.php');
+
+App::bind('database', new QueryBuilder(
+    Connection::make(App::get('config')['database'])
+));
+
+function view($name, $data = [])
+{
+    extract($data);
+    return require "app/views/{$name}.view.php";
+}
+
+function redirect($uri_path)
+{
+    header("Location: /{$uri_path}");
+}
+
+function dd($dumped_val)
+{
+    echo '<pre>';
+    die(var_dump($dumped_val));
+    echo '</pre>';
+}	// dump and die
