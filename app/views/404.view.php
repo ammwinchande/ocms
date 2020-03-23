6eae0eb54d3e1f18cc7617c861628214cb6975c2
Muhammad A. Mwinchande <ammwinchande@gmail.com>
2020-03-24 00:32:08 +0300

    added 404 page

---
+++ app/views/404.view.php
@@ -0,0 +1,16 @@
+<?php require 'partials/header.php'; ?>
+
+
+    <div class="page-wrap d-flex flex-row align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-12 col-md-12 text-center text-danger">
+                    <span class="display-1 d-block" style="font-size: 16rem;">404</span>
+                    <div class="mb-4 lead">The page you are looking for was not found.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<?php require 'partials/footer.php';
